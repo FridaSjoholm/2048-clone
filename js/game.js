ac778a43948a3796f4e9a28c2d877e55ae3d552c
Andrew Bruckman & Frida Sjoholm & Joe Viveiros <pair+abruckman.fridasjoholm.jviveiros@devbootcamp.com>
2017-01-09 17:13:13 -0800

    move the tiles

--- js/game.js
+++ js/game.js
@@ -1,4 +1,4 @@
-var Game = function(board = '00200000000000000020'){
+var Game = function(board = '2022200232020002'){
   var prelBoard= _.map(_.toArray(board), function(num){
     return parseInt(num, 10)
   })
@@ -18,12 +18,84 @@ Game.prototype.toString = function() {
 }
 
 Game.prototype.move = function(direction){
-  if (direction === 'right'){
-    for (y = 0; y<4; y++) {
-      for (x = 0, x <4, x++){})
 
-      })
+  for (i = 0; i<4; i++) {
+    var array = this.arrayCreator(direction, i)
+    var workingArray = filterZeros(array);
+    workingArray = combineLikes(workingArray);
+    workingArray = addZeroes(workingArray);
+    this.saveChanges(workingArray, direction, i)
+  }
+}
+
+Game.prototype.arrayCreator = function(direction, i){
+  if (direction === 'right'){
+    return [this.board[i][0], this.board[i][1], this.board[i][2], this.board[i][3]]
+  }else if (direction === 'left'){
+    return [this.board[i][3], this.board[i][2], this.board[i][1], this.board[i][0]]
+  }else if (direction === 'down'){
+    return [this.board[0][i], this.board[1][i], this.board[2][i], this.board[3][i]]
+  }else if (direction === 'up'){
+    return [this.board[3][i], this.board[2][i], this.board[1][i], this.board[0][i]]
+  }
+}
+
+Game.prototype.saveChanges = function(array, direction, i){
+  if (direction === 'right'){
+    for (j=0; j<=3; j++){
+      this.board[i][j] = array[j];
+    }
+
+  }else if(direction === 'left'){
+    var r=0
+    for(j=3; j>=0; j--){
+      this.board[i][j] = array[r];
+      r++
+    }
+  }else if(direction === 'down'){
+    for (j=0; j<=3; j++){
+      this.board[j][i] = array[j];
+    }
+  }else if(direction === 'up'){
+    var r=0
+    for(j=3; j>=0; j--){
+      this.board[j][i] = array[r];
+      r++
     }
   }
+}
+
+Game.prototype.addNum = function(direction){
 
 }
+
+var filterZeros = function(array){
+  return _.without(array, 0)
+}
+
+var combineLikes = function(array){
+  var newArray =[]
+  for(k = array.length-1; k >= 0; k--){
+    if(k === 0 ){
+      newArray.unshift(array[0])
+    } else{
+      if (array[k] === array[k-1]){
+        newArray.unshift(array[k] + array[k-1])
+        k--
+      }else{
+        newArray.unshift(array[k])
+      }
+    }
+  }
+  return newArray
+}
+
+var addZeroes =function(array){
+  while(array.length < 4){
+    array.unshift(0)
+  }
+  return array
+}
+
+// var actedUponArray =[]
+//  = [board[y][0], board[y][1], board[y][2], board [y][3]]
